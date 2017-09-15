3ee9928d103e17fa31cc0689877f2c961ef8a58e
aladeeb <ahmed.ayman.aladeeb@gmail.com>
2017-09-15 22:29:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Xylophone
+Xylophone is a fun musical instrument anyone can play! You can use it to learn the basics of music.
+
+## Finished App
+TODO
---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Ahmed Ayman on 15/10/2017.
+//  Copyright © 2017 Ahmad Ayman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+  
+
+}
+
